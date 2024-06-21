61b7cdf1402eb1317b9876a956edd15b5c37f596
llcowry <llcowry@qq.com>
2024-06-21 13:38:03 +0800

    Updated config

--- apiserver/config/config.mjs
+++ apiserver/config/config.mjs
@@ -2,17 +2,18 @@ import defaultConfig from './default.mjs';
 import developmentConfig from './development.mjs';
 import productionConfig from './production.mjs';
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'default';
 
-let environmentConfig;
-
+let environmentConfig = {};
+console.log(env);
 switch (env) {
   case 'production':
     environmentConfig = productionConfig;
     break;
   case 'development':
-  default:
     environmentConfig = developmentConfig;
+    break;
+  default:
     break;
 }
 
