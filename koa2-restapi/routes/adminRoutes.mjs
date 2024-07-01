e6cf6eb863fa55b23a7f7e0a018b68362e995073
llcowry <llcowry@qq.com>
2024-07-01 11:59:12 +0800

    Updated something

--- koa2-restapi/routes/adminRoutes.mjs
+++ koa2-restapi/routes/adminRoutes.mjs
@@ -5,6 +5,8 @@ import {
   addAdmin,
   updateAdmin,
   deleteAdmin,
+  setAdminStatus,
+  setAdminPassword,
   adminLogin,
   adminLogout,
   refreshAdminToken,
@@ -17,6 +19,8 @@ router.post('/', addAdmin); // 新增管理员
 router.get('/:id', getAdmin); // 获取管理员
 router.put('/:id', updateAdmin); // 修改管理员
 router.delete('/:id', deleteAdmin); // 删除管理员
+router.post('/:id/status', setAdminStatus); // 设置管理员状态
+router.post('/:id/password', setAdminPassword); // 设置管理员密码
 router.post('/login', adminLogin); // 管理员登录
 router.post('/logout', adminLogout); // 管理员注销
 router.post('/refresh-token', refreshAdminToken); // 刷新管理员token
